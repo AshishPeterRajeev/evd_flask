28c2d04aaa1a3a7cb2ec3cc6ecf29329f6775a53
AshishPeterRajeev <codehub.ash@gmail.com>
2023-04-14 14:58:01 +0530

    modified:   app.py :: added hello

--- app.py
+++ app.py
@@ -16,12 +16,16 @@ def handle_data():
     # Return a response to the client
     return 'Data received'
 
-@app.route('/data', methods=['GET'])
-def get_data():
+# @app.route('/data', methods=['GET'])
+# def get_data():
     
-    time.sleep(6)
-    # Return the data dictionary as JSON
-    return jsonify(data_dict)
+#     time.sleep(6)
+#     # Return the data dictionary as JSON
+#     return jsonify(data_dict)
+
+@app.route('/',methods=['GET'])
+def hello():
+    return jsonify('hello world')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
